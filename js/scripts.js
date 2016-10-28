3de3a36f65b5631a5d4d8c72fb1636651424f334
Erik Killops <erik.killops@gmail.com>
2016-10-28 08:25:25 -0700

    add jquery line to empty output list before adding new result, for replayability

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
 
     var userNumberInput = $("#numberInput").val();
     var result = checkInput(userNumberInput);
+    $("#pingPongResult").empty();
     $("#pingPongResult").append("<li>"+result+"</li");
 
   });
